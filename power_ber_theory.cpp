e4073327c1a9a3debd48f61d910a9aced063166e
TsunakawaShunya <uu.tsunakawa@gmail.com>
2024-09-27 16:39:01 +0900

    Eb/N0-BER上界一般化

--- power_ber_theory.cpp
+++ power_ber_theory.cpp
@@ -1,31 +1,87 @@
 // SNに対するBER
+/*  
+    最適な回転角
+    QPSK: 27.3678°
+    16QAM: 21.0148°
+    64QAM: 20.9291°
+    256QAM: 21.0098°
+*/
 #include "simulator.h"
 #include <vector>
 
 // SNR
-static const double EbN0dBmin = 0.0;                    // Eb/N0 の最小値 [dB]
-static const double EbN0dBmax = 40.0;                   // Eb/N0 の最大値 [dB]
-static const double EbN0dBstp = 0.5;                    // Eb/N0 の間隔 [dB]
-double EbN0dB;
+static const double EbN0dBmin = 0.0;   // Eb/N0 の最小値 [dB]
+static const double EbN0dBmax = 40.0;  // Eb/N0 の最大値 [dB]
+static const double EbN0dBstp = 0.5;   // Eb/N0 の間隔 [dB]
 
 // 回転角
 double theta_deg;
 double theta;
 
-// ファイル
-std::string filenameUpperBoundSum;                      // 厳密な BER 上界の合計
-std::vector<std::string> filenameUpperBound;            // 誤りビットごとの厳密な BER 上界の合計
-std::string filenameNearlyUpperBoundSum;                // BER 上界近似の合計
-std::vector<std::string> filenameNearlyUpperBound;      // 誤りビット数ごとの BER 上界近似
-
-std::ofstream ofsUpperBoundSum;                         // 厳密な BER 上界の合計
-std::vector<std::ofstream> ofsUpperBound;               // 誤りビット数ごとの 厳密な BER 上界
-std::ofstream ofsNearlyUpperBoundSum;                   // BER 上界近似の合計
-std::vector<std::ofstream> ofsNearlyUpperBound;         // 誤りビット数ごとのBER 上界近似
+// ファイル名およびストリーム
+std::ofstream ofsUpperBoundSum;
+std::vector<std::ofstream> ofsUpperBound;
+std::ofstream ofsNearlyUpperBoundSum;
+std::vector<std::ofstream> ofsNearlyUpperBound;
 
 // BER
-Eigen::VectorXd berUpperBoundVec;                       // BER上界
-Eigen::VectorXd berNearlyUpperBoundVec;                 // BER上界の近似値
+Eigen::VectorXd berUpperBoundVec;
+Eigen::VectorXd berNearlyUpperBoundVec;
+
+// ファイル作成の関数
+void initializeFiles(Simulator& sim, std::ofstream& ofsUpperBoundSum,
+                std::ofstream& ofsNearlyUpperBoundSum, 
+                std::vector<std::ofstream>& ofsUpperBound, 
+                std::vector<std::ofstream>& ofsNearlyUpperBound) {
+    // 2^NUMBER_OF_BITの計算 (QAMの階数)
+    int modulationOrder = static_cast<int>(std::pow(2, sim.NUMBER_OF_BIT));
+
+    // ファイル名の作成
+    std::string filenameUpperBoundSum = "Rotated" + std::to_string(modulationOrder) + "QAM_upperbound_" + std::to_string(theta_deg) + "deg.csv";
+    ofsUpperBoundSum.open(filenameUpperBoundSum);
+
+    std::string filenameNearlyUpperBoundSum = "Rotated" + std::to_string(modulationOrder) + "QAM_nearly_upperbound_" + std::to_string(theta_deg) + "deg.csv";
+    ofsNearlyUpperBoundSum.open(filenameNearlyUpperBoundSum);
+
+    // 各ビットごとのファイル名を作成
+    for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
+        std::string filenameUpperBound = "Rotated" + std::to_string(modulationOrder) + "QAM_upperbound_" + std::to_string(theta_deg) + "deg.csv" + std::to_string(i + 1) + ".csv";
+        ofsUpperBound[i].open(filenameUpperBound);
+
+        std::string filenameNearlyUpperBound = "Rotated" + std::to_string(modulationOrder) + "QAM_nearly_upperbound_" + std::to_string(theta_deg) + "deg.csv" + std::to_string(i + 1) + ".csv";
+        ofsNearlyUpperBound[i].open(filenameNearlyUpperBound);
+    }
+}
+
+// Eb/N0 でループし、結果を出力およびファイル書き込み
+void process(Simulator& sim, std::ofstream& ofsUpperBoundSum, std::ofstream& ofsNearlyUpperBoundSum,
+                  std::vector<std::ofstream>& ofsUpperBound, std::vector<std::ofstream>& ofsNearlyUpperBound) {
+    for (double EbN0dB = EbN0dBmin; EbN0dB <= EbN0dBmax; EbN0dB += EbN0dBstp) {
+        sim.setNoiseSD(EbN0dB);
+
+        // BER の取得
+        berUpperBoundVec = sim.getBerUpperBoundVec(theta);
+        berNearlyUpperBoundVec = sim.get_nearlyBerUpperBoundVec(theta);
+
+        // 標準出力
+        std::cout << "--------------------------------------------" << std::endl;
+        for (int i = 0; i < sim.NUMBER_OF_BIT; ++i) {
+            std::cout << "upperbound(" << i << ") : " << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
+            std::cout << "nearly_upperbound(" << i << ") : " << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
+        }
+        std::cout << "upperbound : " << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
+        std::cout << "nearly_upperbound : " << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
+        std::cout << "--------------------------------------------" << std::endl;
+
+        // ファイル書き込み
+        for (int i = 0; i < sim.NUMBER_OF_BIT; ++i) {
+            ofsUpperBound[i] << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
+            ofsNearlyUpperBound[i] << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
+        }
+        ofsUpperBoundSum << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
+        ofsNearlyUpperBoundSum << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
+    }
+}
 
 int main() {
     Simulator sim;
@@ -38,112 +94,23 @@ int main() {
     theta = theta_deg * M_PI / 180;
 
     // シンボル設計
-    sim.setSymbol();        // 従来QAMでのシンボル設計
-    sim.setRotationSymbol(theta);       // 回転
-
+    sim.setSymbol();
+    sim.setRotationSymbol(theta);
 
     // 配列の初期化
-    filenameUpperBound.resize(sim.NUMBER_OF_BIT);
-    ofsUpperBound.resize(sim.NUMBER_OF_BIT);
+    std::vector<std::ofstream> ofsUpperBound(sim.NUMBER_OF_BIT);
+    std::vector<std::ofstream> ofsNearlyUpperBound(sim.NUMBER_OF_BIT);
     berUpperBoundVec.resize(sim.NUMBER_OF_BIT);
-    filenameNearlyUpperBound.resize(sim.NUMBER_OF_BIT);
-    ofsNearlyUpperBound.resize(sim.NUMBER_OF_BIT);
     berNearlyUpperBoundVec.resize(sim.NUMBER_OF_BIT);
 
-    switch(sim.NUMBER_OF_BIT) {
-        case 2:
-            // ファイル作成
-            filenameUpperBoundSum = "RotatedQPSK_upperbound_" + std::to_string(theta_deg) + "deg.csv";
-            ofsUpperBoundSum.open(filenameUpperBoundSum);
-            filenameNearlyUpperBoundSum = "RotatedQPSK_nearly_upperbound_" + std::to_string(theta_deg) + "deg.csv";
-            ofsNearlyUpperBoundSum.open(filenameNearlyUpperBoundSum);
+    std::ofstream ofsUpperBoundSum;
+    std::ofstream ofsNearlyUpperBoundSum;
 
-            for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                filenameUpperBound[i] = "RotatedQPSK_upperbound_" + std::to_string(theta_deg) + "deg_" + std::to_string(i + 1) + ".csv";
-                ofsUpperBound[i].open(filenameUpperBound[i]);
-                filenameNearlyUpperBound[i] = "RotatedQPSK_nearly_upperbound_" + std::to_string(theta_deg) + "deg_" + std::to_string(i + 1) + ".csv";
-                ofsNearlyUpperBound[i].open(filenameNearlyUpperBound[i]);
-            }
+    // ファイル生成と処理の呼び出し
+    initializeFiles(sim, ofsUpperBoundSum, ofsNearlyUpperBoundSum, ofsUpperBound, ofsNearlyUpperBound);
+    process(sim, ofsUpperBoundSum, ofsNearlyUpperBoundSum, ofsUpperBound, ofsNearlyUpperBound);
 
-            // Eb/N0[dB]でループ
-            for(double EbN0dB = EbN0dBmin; EbN0dB <= EbN0dBmax; EbN0dB += EbN0dBstp) {
-                sim.set_QPSKNoiseSD(EbN0dB);
-
-                berUpperBoundVec = sim.getBerUpperBoundVec(theta);
-                berNearlyUpperBoundVec = sim.get_nearlyBerUpperBoundVec(theta);
-
-                // 標準出力
-                std::cout << "--------------------------------------------" << std::endl;
-                for(int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    std::cout << "upperbound(" << i << ") : " << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
-                }
-                std::cout << "upperbound : " << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
-                for(int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    std::cout << "nearly_upperbound(" << i << ") : " << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
-                }
-                std::cout << "nearly_upperbound : " << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
-                std::cout << "--------------------------------------------" << std::endl;
-
-                // ファイル出力
-                for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    ofsUpperBound[i] << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
-                }
-                ofsUpperBoundSum << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
-
-                for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    ofsNearlyUpperBound[i] << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
-                }
-                ofsNearlyUpperBoundSum << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
-            }
-        break;
-        case 4:
-            // ファイル作成
-            filenameUpperBoundSum = "Rotated16QAM_upperbound_" + std::to_string(theta_deg) + "deg.csv";
-            ofsUpperBoundSum.open(filenameUpperBoundSum);
-            filenameNearlyUpperBoundSum = "Rotated16QAM_nearly_upperbound_" + std::to_string(theta_deg) + "deg.csv";
-            ofsNearlyUpperBoundSum.open(filenameNearlyUpperBoundSum);
-
-            for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                filenameUpperBound[i] = "Rotated16QAM_upperbound_" + std::to_string(theta_deg) + "deg_" + std::to_string(i + 1) + ".csv";
-                ofsUpperBound[i].open(filenameUpperBound[i]);
-                filenameNearlyUpperBound[i] = "Rotated16QAM_nearly_upperbound_" + std::to_string(theta_deg) + "deg_" + std::to_string(i + 1) + ".csv";
-                ofsNearlyUpperBound[i].open(filenameNearlyUpperBound[i]);
-            }
-            
-            
-            // Eb/N0[dB]でループ
-            for(double EbN0dB = EbN0dBmin; EbN0dB <= EbN0dBmax; EbN0dB += EbN0dBstp) {
-                sim.set_16QAMNoiseSD(EbN0dB);
-
-                berUpperBoundVec = sim.getBerUpperBoundVec(theta);
-                berNearlyUpperBoundVec = sim.get_nearlyBerUpperBoundVec(theta);
-
-                // 標準出力
-                std::cout << "--------------------------------------------" << std::endl;
-                for(int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    std::cout << "upperbound(" << i << ") : " << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
-                    std::cout << "upperbound(" << i << ") : " << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
-                }
-                std::cout << "upperbound : " << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
-                for(int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    std::cout << "nearly_upperbound(" << i << ") : " << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
-                }
-                std::cout << "nearly_upperbound : " << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
-               
-                // ファイル出力
-                for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    ofsUpperBound[i] << EbN0dB << "," << berUpperBoundVec(i) << std::endl;
-                }
-                ofsUpperBoundSum << EbN0dB << "," << berUpperBoundVec.sum() << std::endl;
-
-                for (int i = 0; i < sim.NUMBER_OF_BIT; i++) {
-                    ofsNearlyUpperBound[i] << EbN0dB << "," << berNearlyUpperBoundVec(i) << std::endl;
-                }
-                ofsNearlyUpperBoundSum << EbN0dB << "," << berNearlyUpperBoundVec.sum() << std::endl;
-            }
-        break;
-    }
-
+    // ファイルを閉じる
     ofsUpperBoundSum.close();
     for (int i = 0; i < sim.NUMBER_OF_BIT; ++i) {
         ofsUpperBound[i].close();
@@ -152,6 +119,6 @@ int main() {
     for (int i = 0; i < sim.NUMBER_OF_BIT; ++i) {
         ofsNearlyUpperBound[i].close();
     }
-    
+
     return 0;
 }
